3f4e4f6f665a5f6ce1b08f041a42f5a954af48ef
Michael <99michelrussell99@gmail.com>
2024-06-16 09:59:52 -0400

    added try except for when page fails to load

--- osrs-music-ripper.py
+++ osrs-music-ripper.py
@@ -8,7 +8,7 @@ from selenium.webdriver import ActionChains
 from chromedriver_py import binary_path
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless=new')
+#chrome_options.add_argument('--headless=new')
 chrome_options.page_load_strategy = 'eager'
 
 svc = webdriver.ChromeService(executable_path=binary_path)
@@ -16,7 +16,7 @@ driver = webdriver.Chrome(service=svc, options=chrome_options)
 
 driver.get("https://oldschool.runescape.wiki/w/Music")
 actionChains = ActionChains(driver)
-wait = WebDriverWait(driver, 30)
+wait = WebDriverWait(driver, 10)
 
 links = []
 hrefs = driver.find_elements(By.LINK_TEXT, "Play track")
@@ -29,7 +29,15 @@ song_links = tqdm(links, bar_format='{desc}{percentage:3.0f}% |{bar}| {n_fmt}/{t
 for link in song_links:
     song_links.set_description("Downloading Song - " + link.split("File:")[1])
     driver.get(link)
-    download = wait.until(EC.visibility_of_element_located((By.XPATH,"/html/body/div[3]/div[3]/div[5]/div[2]/p/a[2]")))
+
+    while(True):
+        try:
+            download = wait.until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[3]/div[3]/div[5]/div[2]/p/a[2]")))
+            break
+        except:
+            driver.refresh()
+            continue
+
     download.click()
 
 time.sleep(5)
