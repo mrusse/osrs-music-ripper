278af70381a2b8d413799255eaa00c1902666cb0
mrusse <99michaelrussell99@gmail.com>
2024-06-16 21:33:09 -0400

    metadata is now being applied

--- osrs-music-ripper.py
+++ osrs-music-ripper.py
@@ -1,4 +1,5 @@
 import requests
+import music_tag
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 from urllib.request import unquote
@@ -13,12 +14,21 @@ hrefs = soup.find_all('a', href=True)
 hrefs = [link['href'] for link in hrefs if link['href'].endswith('.ogg')]
 title_max_length = len(max(hrefs, key = len).split("File:")[1])
 
-for href in (bar := tqdm(hrefs, desc = "Downloading: ", bar_format='{desc}{percentage:3.0f}% |{bar}| {n_fmt}/{total_fmt}')):
+for i, href in enumerate(bar := tqdm(hrefs, desc = "Downloading: ", bar_format='{desc}{percentage:3.0f}% |{bar}| {n_fmt}/{total_fmt}')):
     title = href.split("File:")[1]
     link = "https://oldschool.runescape.wiki/images/" + title
 
+    composer = requests.get("https://oldschool.runescape.wiki/w/" + title.replace(".ogg",""))
+
+    if ("Composer" in composer.text):
+        composer = composer.text.split("Composer")[1].split("\n")[2].split(">")[1].replace("<small","")
+    else:
+        composer = requests.get("https://oldschool.runescape.wiki/w/" + title.replace(".ogg","") + "_(music_track)")
+        composer = composer.text.split("Composer")[1].split("\n")[2].split(">")[1].replace("<small","")
+
     title = unquote(title)
     bar.set_description("Downloading Song - " + str(title).ljust(title_max_length))
+    title = str(title).replace("_", " ")
     
     req = Request(
         url=link, 
@@ -26,5 +36,16 @@ for href in (bar := tqdm(hrefs, desc = "Downloading: ", bar_format='{desc}{perce
     )
 
     oggfile = urlopen(req)
-    with open("music\\" + str(title),'wb') as output:
+    with open("music\\" + title,'wb') as output:
         output.write(oggfile.read())
+    
+    song = music_tag.load_file("music\\" + title)
+    song['title'] = title.split(".ogg")[0]
+    song['album'] = "Unofficial Old School RuneScape Soundtrack"
+    song['artist'] = composer
+    song.raw['tracknumber'] = str(i+1).zfill(3)
+
+    with open('cover.jpg', 'rb') as cover:
+        song['artwork'] = cover.read()
+    
+    song.save()
